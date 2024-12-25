8344e3168f1597998f05ea11dc02601d32abd9ff
ShlKan <qianqianli198808@gmail.com>
2024-12-25 14:09:00 +0100

    feat: finish process generation

--- IRAnalysis/CodeGen/SysGenModule.h
+++ IRAnalysis/CodeGen/SysGenModule.h
@@ -9,8 +9,6 @@
 #include "clang/AST/Expr.h"
 #include "clang/Basic/Diagnostic.h"
 #include "llvm/ADT/SmallVector.h"
-#include "llvm/ADT/StringRef.h"
-#include <string>
 
 #include "CPPFrontend/CIROptions.h"
 
@@ -40,11 +38,37 @@ private:
   mlir::ModuleOp theModule;
   mlir::OpBuilder builder;
 
+private:
+  void collectProcess(clang::CXXRecordDecl *moduleDecl);
+
 public:
   mlir::ModuleOp getModule() { return theModule; }
   clang::DiagnosticsEngine &getDiag() { return diags; }
   const cir::CIROptions &getCIROptions() { return cirOptions; }
   void buildSysModule(clang::CXXRecordDecl *moduleDecl);
+
+  mlir::Location getLoc(clang::SourceLocation SLoc) {
+    assert(SLoc.isValid() && "expected valid source location");
+    const clang::SourceManager &SM = astCtx.getSourceManager();
+    clang::PresumedLoc PLoc = SM.getPresumedLoc(SLoc);
+    llvm::StringRef Filename = PLoc.getFilename();
+    return mlir::FileLineColLoc::get(builder.getStringAttr(Filename),
+                                     PLoc.getLine(), PLoc.getColumn());
+  }
+
+  mlir::Location
+  getLoc(clang::SourceRange SLoc) { // Some AST nodes might contain invalid
+                                    // source locations (e.g.
+    // CXXDefaultArgExpr), workaround that to still get something out.
+    if (SLoc.isValid()) {
+      mlir::Location B = getLoc(SLoc.getBegin());
+      mlir::Location E = getLoc(SLoc.getEnd());
+      llvm::SmallVector<mlir::Location, 2> locs = {B, E};
+      mlir::Attribute metadata;
+      return mlir::FusedLoc::get(locs, metadata, builder.getContext());
+    }
+    return builder.getUnknownLoc();
+  };
 };
 } // namespace sys
 

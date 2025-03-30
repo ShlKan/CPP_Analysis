e92650b84963710976eeac2127ecc7bd8c1c54d5
Shuanglong Kan <kanshuanglong@outlook.com>
2025-03-30 08:17:40 +0200

    modify tests

--- IRAnalysis/test/basic_constructs/assignNameSpace.cpp
+++ IRAnalysis/test/basic_constructs/assignNameSpace.cpp
@@ -2,10 +2,12 @@
 
 #include <systemc>
 
+using namespace sc_dt;
+
 SC_MODULE(Process) {
   int x = 123;
-  sc_dt::sc_int<33> y = 1;
-  sc_dt::sc_int<33> z = y + 1;
+  sc_int<33> y = 1;
+  sc_int<33> z = y + 1;
   // CHECK: %2 = "cir.const"() <{value = #cir.int<1> : !u32i}> : () -> !u32i
   // CHECK: %3 = "sys.sbinop"(%1, %2) <{kind = 4 : i32}> {sym_name = "z"} :
   // (!sys.s_int<s, 33>, !u32i) -> !sys.s_int<s, 33>
--- IRAnalysis/test/basic_constructs/simpleProcessIf.cpp
+++ IRAnalysis/test/basic_constructs/simpleProcessIf.cpp
@@ -2,20 +2,11 @@
 
 #include <systemc>
 
-// CHECK1: !s32i = !cir.int<s, 32>
-// CHECK1: !u32i = !cir.int<u, 32>
-SC_MODULE(Process){// CHECK: "builtin.module"() <{sym_name = "Process"}> ({
-                   SC_CTOR(Process){SC_THREAD(emptyProcess);
-// CHECK: %0 = "sys.ProcDef"() <{proc_name = "emptyProcess", proc_type =
-// !sys<s_proc Proc ()>}> ({
+SC_MODULE(Process){SC_CTOR(Process){SC_THREAD(emptyProcess);
 }
 void emptyProcess() {
   sc_dt::sc_int<8> i = 2;
-  // CHECK:  %1 = "sys.const"() <{value = #sys.int<2> : !sys.s_int<s, 8>}> : ()
-  // -> !sys.s_int<s, 8>
   sc_dt::sc_int<8> j = 3;
-  // CHECK: %2 = "sys.const"() <{value = #sys.int<3> : !sys.s_int<s, 8>}> : ()
-  // -> !sys.s_int<s, 8>
   if (i > j) {
     sc_dt::sc_int<8> x = 0;
   }
@@ -26,9 +17,7 @@ void emptyProcess() {
   // CHECK: ^bb1: // pred: ^bb0
   // CHECK: %4 = "sys.const"() <{value = #sys.int<0> : !sys.s_int<s, 8>}> : ()
   // -> !sys.s_int<s, 8>
-  // CHECK: }) : () -> !sys<s_proc Proc ()>
-  // CHECK: "sys.ProcRegister"(%0) : (!sys<s_proc Proc ()>) -> ()
-  // CHECK: }) : () -> ()
+  // CHECK: ^bb2:  // pred: ^bb0
 }
 }
 ;
